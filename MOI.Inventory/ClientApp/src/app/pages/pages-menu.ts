5858e0d6dd110bcd8521d2960c23b5e279402a9f
Mohd Imran <mohdimran043@gmail.com>
2019-03-29 10:13:16 +0300

    Add project files.

---
+++ MOI.Inventory/ClientApp/src/app/pages/pages-menu.ts
@@ -0,0 +1,20 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'Inventory Stats',
+    icon: 'nb-e-commerce',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'Add Inventory',
+    icon: 'nb-home',
+    link: '/pages/iot-dashboard',
+  },
+  {
+    title: 'Dispatch Inventory',
+    icon: 'nb-home',
+    link: '/pages/iot-dashboard',
+  }
+];
